f05b2e800100c49f9e0d0fc1a2387f2dc645997f
root <root@localhost.localdomain>
2020-12-06 21:13:15 +0530

    added uploading gif

--- handlers/multer.js
+++ handlers/multer.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 module.exports = multer({
   storage : multer.diskStorage({}),
   fileFilter : (req,file,cb) =>{
-    if(!file.mimetype.match(/jpg|jpeg|png|gif$i/)){
+    if(!file.mimetype.match(/jpg|jpeg|png|gif|webp|svg$i/)){
       cb(new Error("File not supported", false))
       return;
     }
