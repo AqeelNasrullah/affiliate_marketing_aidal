6cc8064115faf6927ee293c0614d7ab6fa8c9934
root <root@localhost.localdomain>
2020-12-06 18:40:55 +0530

    added uploading gif

--- public/main.js
+++ public/main.js
@@ -1,3 +1,7 @@
 function showthumb(){
   document.getElementsByClassName('form-group')[0].style.display = "block";
 }
+
+function dis(){
+  document.getElementsByClassName("uploading")[0].style.display="block";
+}
