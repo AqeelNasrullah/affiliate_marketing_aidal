a6d4922f2f8a6f2e0d673b076568f81db5e819be
RanaDanyal <74705350+RanaDanyal@users.noreply.github.com>
2021-02-20 12:42:04 +0500

    a

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ app.get("/storieslist", verify, (req, res) => {
 });
 
 app.get('/learnmore', (req, res) => {
-  res.sendFile(__dirname+"/learnmore.ejs")
+  res.sendFile(__dirname+learnmore.ejs)
 });
 
 app.get("/:name", (req, res) => {
